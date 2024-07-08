cff0b2f5525b0036718348f5a71a607bc293bbd7
Joshua Peralta <peralta.joshuapaul07@gmail.com>
2024-07-08 15:39:56 +1200

    update app.js

---
+++ src/components/NumberSumUp.js
@@ -0,0 +1,55 @@
+import React, {useState} from 'react';
+import {BaseUrl} from "../consistents";
+import axios from "axios";
+
+function NumberSumUp(props) {
+    const [startNum, setStartNum] = useState(0)
+    const [endNum, setEndNum] = useState(0)
+    const [result, setResult] = useState(0)
+
+    function startNumHandler(e) {
+        setStartNum(e.target.value)
+    }
+
+    function endNumHandler(e) {
+        setEndNum(e.target.value)
+    }
+
+    function calculateHandler() {
+        let data = JSON.stringify({
+            "start_num": {startNum},
+            "end_num": {endNum}
+        });
+
+        let config = {
+            method: 'post',
+            maxBodyLength: Infinity,
+            url: BaseUrl + '/chat/sum_numbers/',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            data: data
+        };
+
+        axios.request(config)
+            .then((response) => {
+                console.log(JSON.stringify(response.data));
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+
+    return (
+        <div>
+            <p>Start Number: <input type={"number"} id={"startNum"} onChange={startNumHandler}/></p>
+            <p>End Number: <input type={"number"} id={"endNum"} onChange={endNumHandler}/></p>
+            <p>
+                <button id={"calculate"} onClick={calculateHandler}>Calculate</button>
+            </p>
+            <p>Result: {result}</p>
+        </div>
+    );
+}
+
+export default NumberSumUp;
